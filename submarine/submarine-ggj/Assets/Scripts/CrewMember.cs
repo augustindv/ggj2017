164e5deacc951a7df9a2b5f9ec629f6774a41659
Nikolaj Leischner <nikolaj.leischner@gmail.com>
2017-01-20 23:33:56 +0100

    Added basic crew & player script + prefabs.

---
+++ submarine/submarine-ggj/Assets/Scripts/CrewMember.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrewMember : MonoBehaviour {
+
+	public static readonly string TAG_LADDER = "Ladder";
+
+	public float speed = 2.5f;
+
+	public float climbSpeed = 1.5f;
+
+	private Rigidbody rigidBody;
+
+	private int collidingLadders = 0;
+
+	void Start () {
+		rigidBody = GetComponentInChildren<Rigidbody> ();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == TAG_LADDER) {
+			UpdateLadders (1);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == TAG_LADDER) {
+			UpdateLadders (-1);
+		}
+	}
+
+	bool IsOnLadder() {
+		return collidingLadders > 0;
+	}
+
+	private void UpdateLadders(int difference) {
+		collidingLadders += difference;
+		rigidBody.useGravity = collidingLadders == 0;
+		Debug.Log ("Update ladders (" + difference + ") -> " + collidingLadders);
+	}
+
+	public void Act() {
+		
+	}
+
+	private float CurrentSpeed() {
+		float currentSpeed = IsOnLadder () ? climbSpeed : speed;
+		return currentSpeed * Time.fixedDeltaTime;
+	}
+
+	public void Move(int horizontal, int vertical) {
+		if (!IsOnLadder ())
+			vertical = 0;
+
+		var currentPosition = rigidBody.transform.position;
+
+		var movement = new Vector3 (horizontal * CurrentSpeed(), vertical * CurrentSpeed(), 0);
+
+		rigidBody.MovePosition (currentPosition + movement);
+	}
+}
---
+++ submarine/submarine-ggj/Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public CrewMember[] crewMembers;
+
+	int activeCrewMember = 0;
+
+	public KeyCode moveLeft = KeyCode.LeftArrow;
+	public KeyCode moveRight = KeyCode.RightArrow;
+	public KeyCode moveUp = KeyCode.UpArrow;
+	public KeyCode moveDown = KeyCode.DownArrow;
+
+	public KeyCode act = KeyCode.Space;
+
+	public KeyCode toggleNextCrew = KeyCode.Alpha1;
+	public KeyCode togglePreviousCrew = KeyCode.Alpha2;
+
+	CrewMember ActiveCrewMember() {
+		return crewMembers [activeCrewMember];
+	}
+
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		if (Input.GetKeyDown (toggleNextCrew))
+			NextCrewMember ();
+		else if (Input.GetKeyDown (togglePreviousCrew))
+			PreviousCrewMember ();
+		else if (Input.GetKeyDown (act))
+			PerformAction ();
+		else {
+			int horizontal = 0;
+			if (Input.GetKey (moveRight))
+				horizontal = 1;
+			else if (Input.GetKey (moveLeft))
+				horizontal = -1;
+
+			int vertical = 0;
+			if (Input.GetKey (moveUp))
+				vertical = 1;
+			else if (Input.GetKey (moveDown))
+				vertical = -1;
+
+			Move (horizontal, vertical);
+		}
+	}
+
+	void NextCrewMember() {
+		if (activeCrewMember == crewMembers.Length - 1)
+			activeCrewMember = 0;
+		else
+			activeCrewMember += 1;
+	}
+
+	void PreviousCrewMember() {
+		if (activeCrewMember == 0)
+			activeCrewMember = crewMembers.Length - 1;
+		else
+			activeCrewMember -= 1;
+	}
+
+	void PerformAction() {
+		ActiveCrewMember ().Act ();
+	}
+
+	void Move(int horizontal, int vertical) {
+		ActiveCrewMember ().Move (horizontal, vertical);
+	}
+}
