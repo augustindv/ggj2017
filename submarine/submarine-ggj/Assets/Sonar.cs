05cf57febf167b3cbffe510237aaaf07b4022091
Nikolaj Leischner <nikolaj.leischner@gmail.com>
2017-01-21 13:11:54 +0100

    Added sonar.

--- submarine/submarine-ggj/Assets/Sonar.cs
+++ submarine/submarine-ggj/Assets/Sonar.cs
@@ -4,13 +4,80 @@ using UnityEngine;
 
 public class Sonar : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject lights;
+
+	public Camera mainCamera;
+
+	public Transform sonarCameraPosition;
+
+	public float cameraZoomSpeed = 5;
+
+	public float range = 40f;
+
+	public float speed = 20f;
+
+	private bool active = true;
+
+	private Vector3 defaultCameraPosition;
+
+	private Coroutine cameraMovement = null;
+
+	public void SetActive(bool active) {
+		this.active = active;
+		if (active) {
+			MoveCamera (sonarCameraPosition.position);
+		}
+		else {
+			UpdateLightPosition (0);
+			MoveCamera (defaultCameraPosition);
+		}
+		lights.SetActive (active);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void MoveCamera(Vector3 target) {
+		if (cameraMovement != null){
+			StopCoroutine (cameraMovement); 
+			cameraMovement = null;
+		}
+		cameraMovement = StartCoroutine (CameraMovement (target));
+	}
+
+	IEnumerator CameraMovement(Vector3 target) {
+		float epsilon = 0.1f;
+		while (Vector3.Distance (mainCamera.transform.position, target) > speed * Time.deltaTime) {
+			var direction = (target - mainCamera.transform.position).normalized;
+			mainCamera.transform.Translate (direction * speed * Time.deltaTime);
+			yield return new WaitForEndOfFrame ();
+		}
+		mainCamera.transform.position = target;
+		yield return null;
+	}
+
+	void UpdateLightPosition(float x) {
+		var p = lights.transform.localPosition;
+		lights.transform.localPosition = new Vector3 (x, p.y, p.z);
+	}
+
+	float LightPosition() {
+		return lights.transform.localPosition.x;
+	}
+
+	void Start() {
+		defaultCameraPosition = mainCamera.transform.position;
+		SetActive (false);
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Q))
+			SetActive (!active);
 		
+		if (active) {
+			var movement = speed * Time.deltaTime;
+			var newPosition = LightPosition () + movement;
+			if (newPosition > range)
+				newPosition -= range;
+
+			UpdateLightPosition (newPosition);
+		}
 	}
 }
