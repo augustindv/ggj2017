b78a9d9f26d74dab256e03130616272f56ec903c
Nikolaj Leischner <nikolaj.leischner@gmail.com>
2017-01-22 13:44:59 +0100

    Added credits/exit to splash screen, added background.

--- submarine/submarine-ggj/Assets/Scripts/SplashScreen.cs
+++ submarine/submarine-ggj/Assets/Scripts/SplashScreen.cs
@@ -8,7 +8,22 @@ using UnityEngine.EventSystems;
 
 public class SplashScreen : MonoBehaviour {
 
+	public GameObject credits;
+
 	public void StartGame() {
 		SceneManager.LoadScene ("level_design_test");
+	}
+
+	public void ShowCredits() {
+		credits.SetActive (true);
+	}
+
+	public void Exit() {
+		Application.Quit ();
+	}
+
+	void Update() {
+		if (Input.GetButtonDown("Cancel"))
+			credits.SetActive(false);
 	}
 }
